bcd71fb2faf0b5762bfde4fa348bce479ac3c735
Pedro Henrique Lourenço Ferezin <phlferezin03@gmail.com>
2021-10-06 12:24:37 -0300

    animação numeros

--- siteTeste/js/script.js
+++ siteTeste/js/script.js
@@ -1,45 +1,123 @@
-var a = document.querySelector('#a')
-var b = document.querySelector('#b')
-var c = document.querySelector('#c')
+var target1 = document.querySelectorAll('.ha')
 
-a.addEventListener('mouseover', function(){
+var efeito1 = 0;
 
-    var numero = document.querySelector('#a')
-    var min = 1
-    var max = 100
+function anime(){
+    const windowTop = window.scrollY;
+    target1.forEach(function(element){
+        if((windowTop) > element.offsetTop + (window.innerHeight * 0.75)  && efeito1 == 0){
 
-    for(var i = min; i <= max; i++){
-        setTimeout(function(nr){
-            numero.innerHTML = nr;
-        }, i * 1000 / max, i);
-    }
+            let numero = document.querySelector('#a')
+            let min = 1
+            let max = 100
+
+            for(let i = min; i <= max; i++){
+                setTimeout(function(nr){
+                    numero.innerHTML = nr;
+                }, i * 3000 / max, i);
+            } efeito1 = 1;
+        }
+        console.log(element.offsetTop);
+    })
+} 
+
+window.addEventListener('scroll', function(){
+    anime();
+})
+// 
+var target2 = document.querySelectorAll('.he')
+
+var efeito2 = 0;
+
+function anime2(){
+    const windowTop = window.scrollY;
+    target2.forEach(function(element){
+        if((windowTop) > element.offsetTop + (window.innerHeight * 0.75)  && efeito2 == 0){
+
+            let numero = document.querySelector('#b')
+            let min = 1
+            let max = 5000
+
+            for(let i = min; i <= max; i++){
+                setTimeout(function(nr){
+                    numero.innerHTML = nr;
+                }, i * 4000 / max, i);
+            } efeito2 = 1;
+        }
+        console.log(element.offsetTop);
+    })
+} 
+
+window.addEventListener('scroll', function(){
+    anime2();
+})
+// 
+var target3 = document.querySelectorAll('.hi')
+
+var efeito3 = 0;
+
+function anime3(){
+    const windowTop = window.scrollY;
+    target3.forEach(function(element){
+        if((windowTop) > element.offsetTop + (window.innerHeight * 0.75)  && efeito3 == 0){
+
+            let numero = document.querySelector('#c')
+            let min = 0
+            let max = 12    
+
+            for(let i = min; i <= max; i++){
+                setTimeout(function(nr){
+                    numero.innerHTML = nr;
+                }, i * 2000 / max, i);
+            } efeito3 = 1;
+        }
+        console.log(element.offsetTop);
+    })
+} 
+
+window.addEventListener('scroll', function(){
+    anime3();
 })
 
-b.addEventListener('mouseover', function(){
-    var numero = document.querySelector('#b')
-    var min = 1
-    var max = 5000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// b.addEventListener('mouseover', function(){
+//     var numero = document.querySelector('#b')
+//     var min = 1
+//     var max = 5000
     
-    for(var i = min; i <= max; i++){
-        setTimeout(function(nr){
-            numero.innerHTML = nr;
-        }, i * 2000 / max, i);
-    }
+//     for(var i = min; i <= max; i++){
+//         setTimeout(function(nr){
+//             numero.innerHTML = nr;
+//         }, i * 2000 / max, i);
+//     }
 
-})
+// })
 
-c.addEventListener('mouseover', function(){
-    var numero = document.querySelector('#c')
-    var min = 1
-    var max = 12
+// c.addEventListener('mouseover', function(){
+//     var numero = document.querySelector('#c')
+//     var min = 1
+//     var max = 12
     
-    for(var i = min; i <= max; i++){
-        setTimeout(function(nr){    
-            numero.innerHTML = nr;
-        }, i * 700 / max, i);
-    }
+//     for(var i = min; i <= max; i++){
+//         setTimeout(function(nr){    
+//             numero.innerHTML = nr;
+//         }, i * 700 / max, i);
+//     }
 
-})
+// })
 
 
 
